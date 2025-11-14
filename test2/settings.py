ab548ab80356b0c26051fa48a9c0c622f9d2f59c
cheikh Hama <cheikhhama20@gmail.com>
2025-11-14 10:01:28 +0000

    allowed Hosts

--- test2/settings.py
+++ test2/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+ALLOWED_HOSTS = ["*"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
